7414b83dbe54d7df0a0dbcda287e976c29a17354
MunHwan Lee <song46077@naver.com>
2026-08-24 14:24:35 +0900

    initial commit

---
+++ back/src/main/java/com/back/nbe12141team07/Nbe12141Team07Application.java
@@ -0,0 +1,13 @@
+package com.back.nbe12141team07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe12141Team07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe12141Team07Application.class, args);
+    }
+
+}
---
+++ back/src/test/java/com/back/nbe12141team07/Nbe12141Team07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.nbe12141team07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe12141Team07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
