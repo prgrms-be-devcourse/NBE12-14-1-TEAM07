a5d1ba114067af82af5847c74916fca8fc9c8e31
MunHwan Lee <song46077@naver.com>
2026-08-25 12:47:22 +0900

    feat: product 단건 조회 exception 추가 및 적용
    # Conflicts:
    #	back/db_dev.mv.db

--- back/src/main/java/com/back/nbe12141team07/domain/product/service/ProductService.java
+++ back/src/main/java/com/back/nbe12141team07/domain/product/service/ProductService.java
@@ -2,6 +2,7 @@ package com.back.nbe12141team07.domain.product.service;
 
 import com.back.nbe12141team07.domain.product.entity.Product;
 import com.back.nbe12141team07.domain.product.repository.ProductRepository;
+import com.back.nbe12141team07.global.exception.ProductNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,8 +27,9 @@ public class ProductService {
         return productRepository.findAllByOrderByCreateDateDesc();
     }
 
-    public Optional<Product> findById(int id) {
-
-        return productRepository.findById(id);
+    // 커스텀 Exception 사용
+    public Product findById(int id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
     }
 }
