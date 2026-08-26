2917c50e4b1834489add3a499c44760017283c13
globaltoper <globaltoper2@gmail.com>
2026-08-26 10:13:56 +0900

    feat: 주문 삭제 기s

--- back/src/main/java/com/back/nbe12141team07/domain/orders/controller/OrdersController.java
+++ back/src/main/java/com/back/nbe12141team07/domain/orders/controller/OrdersController.java
@@ -1,4 +1,29 @@
 package com.back.nbe12141team07.domain.orders.controller;
 
+import com.back.nbe12141team07.domain.orders.service.OrdersService;
+import com.back.nbe12141team07.global.jpa.entity.dto.RsData;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrdersController {
+
+    private final OrdersService ordersService;
+
+    // DELETE /api/orders/{id} : 주문 삭제
+    @DeleteMapping("/{id}")
+    @Transactional
+    public RsData<Void> delete(@PathVariable int id) {
+        ordersService.deleteOrders(id);
+        return new RsData<>(
+                "200-1",
+                "%d번 주문이 삭제되었습니다.".formatted(id)
+        );
+    }
 }
--- back/src/main/java/com/back/nbe12141team07/domain/orders/service/OrdersService.java
+++ back/src/main/java/com/back/nbe12141team07/domain/orders/service/OrdersService.java
@@ -1,4 +1,30 @@
 package com.back.nbe12141team07.domain.orders.service;
 
+
+import com.back.nbe12141team07.domain.orders.entity.Orders;
+import com.back.nbe12141team07.domain.orders.repository.OrdersRepository;
+import com.back.nbe12141team07.global.exception.OrdersNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class OrdersService {
+
+    private final OrdersRepository ordersRepository;
+
+    //id로 주문 조회, 없으면 custom exception 발생
+    public Orders findById(int id) {
+        return ordersRepository.findById(id)
+                .orElseThrow(() -> new OrdersNotFoundException(id));
+    }
+
+    //주문 삭제
+    //Orders에 cascade = ALL, orphanRemoval = true이 걸려있기 때문에,
+    // Orders를 삭제하면 OrdersDetail도 같이 삭제됨
+    public void deleteOrders(int id) {
+        Orders orders = findById(id);
+
+        ordersRepository.delete(orders);
+    }
 }
