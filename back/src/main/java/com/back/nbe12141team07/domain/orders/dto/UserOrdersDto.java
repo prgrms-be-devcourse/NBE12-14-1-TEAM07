cc9a9c9f84b25d97e3bde48f4412355d38b202da
202244067 <dongjunjwa4020@naver.com>
2026-08-27 17:25:51 +0900

    사용자 주문 조회 응답에 시각 추가

--- back/src/main/java/com/back/nbe12141team07/domain/orders/dto/UserOrdersDto.java
+++ back/src/main/java/com/back/nbe12141team07/domain/orders/dto/UserOrdersDto.java
@@ -3,15 +3,18 @@ package com.back.nbe12141team07.domain.orders.dto;
 import com.back.nbe12141team07.domain.orders.entity.OrderDetailStatus;
 import com.back.nbe12141team07.domain.orders.entity.Orders;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record UserOrdersDto(
         int id,
+        LocalDateTime modifyDate,
         List<OrdersDetailDto> ordersDetails
 ) {
     public UserOrdersDto(Orders orders) {
         this(
                 orders.getId(),
+                orders.getModifyDate(),
                 orders.getOrdersDetails()
                         .stream()
                         .filter(detail ->
